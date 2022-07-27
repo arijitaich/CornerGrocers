2f6f56b15f09d1f2dfa25ff5d2e114c3b11afbb7
adarsh-chouhan-au8 <adarsh.chouhan11@gmail.com>
2022-07-28 01:52:36 +0530

    'upload'

--- utils/func.js
+++ utils/func.js
@@ -7,11 +7,11 @@ const sleep = (ms) => {
 const getJson = async (rows) => {
   const result = [];
   let temp = {
-    Category: '',
-    Items:[]
+    Category: "",
+    Items: [],
   };
-  let subItemsArr = []
-  let saveItem = true
+  let subItemsArr = [];
+  let saveItem = true;
   const CATEGORY = "Category";
   const RANK = "Rank";
   const MODIFIER_NAME = "Modifier Name";
@@ -22,77 +22,60 @@ const getJson = async (rows) => {
   const UPC = "UPC#";
   const ITEMS_PRICE = "Items Price";
 
-  for (i=0;i<rows.length;i++) {
-     console.log(temp[CATEGORY],rows[i][CATEGORY]);
-
-    // if (rows[i][CATEGORY]== undefined) {continue;}
-
-    if (rows[i][CATEGORY] != '' && i!=0) {
-      //  console.log(temp[ITEMS].length)
+  for (i = 0; i < rows.length; i++) {
+    if (rows[i][CATEGORY] != "" && i != 0) {
       result.push(temp);
-     // console.log(result)
       temp = {
-        Category: '',
-        Items:[]
+        Category: "",
+        Items: [],
       };
-    //   continue;
     }
 
-    if (!temp[CATEGORY])  temp[CATEGORY] = rows[i][CATEGORY];
+    if (!temp[CATEGORY]) temp[CATEGORY] = rows[i][CATEGORY];
 
+    if (!rows[i][BASE_PRICE]) {
+      // if subitems are present
+      if (saveItem) {
+        subItem = {
+          MODIFIER_NAME: rows[i][MODIFIER_NAME],
+          DESCRIPTION: rows[i][DESCRIPTION],
+          BASE_PRICE: rows[i][BASE_PRICE],
+          SUB_MODIFIER_NAME: rows[i][SUB_MODIFIER_NAME],
+          UPC: rows[i][UPC],
+        };
+      }
 
-    if (!rows[i][BASE_PRICE]) // subitems are present
-    {
-        if (saveItem){
-            subItem = {
-                MODIFIER_NAME: rows[i][MODIFIER_NAME],
-                DESCRIPTION : rows[i][DESCRIPTION],
-                BASE_PRICE : rows[i][BASE_PRICE],
-                SUB_MODIFIER_NAME : rows[i][SUB_MODIFIER_NAME],      
-                UPC : rows[i][UPC],    
-            }
-        }
+      saveItem = false;
 
-        saveItem = false
+      subItemsArr.push({
+        item: rows[i][ITEMS],
+        price: rows[i][ITEMS_PRICE],
+      });
 
-        subItemsArr.push({
-            item: rows[i][ITEMS],
-            price: rows[i][ITEMS_PRICE]
-        })
+      if (rows[i + 1][MODIFIER_NAME] != "") {
+        subItem[ITEMS] = subItemsArr;
+        temp[ITEMS].push(subItem);
+        subItemsArr = [];
+        saveItem = true;
+      }
 
-        if (rows[i+1][MODIFIER_NAME]!='' )
-        {
-            subItem[ITEMS] = subItemsArr
-            temp[ITEMS].push(subItem)
-            subItemsArr = []
-            saveItem = true
-            
-        }
-  
-        continue
-        
-
+      continue;
     }
 
     item = {
-        MODIFIER_NAME: rows[i][MODIFIER_NAME],
-        DESCRIPTION : rows[i][DESCRIPTION],
-        BASE_PRICE : rows[i][BASE_PRICE],
-        SUB_MODIFIER_NAME : rows[i][SUB_MODIFIER_NAME],
-        ITEMS : rows[i][ITEMS],
-        UPC : rows[i][UPC],
-        ITEMS_PRICE : rows[i][ITEMS_PRICE],
-    }
+      MODIFIER_NAME: rows[i][MODIFIER_NAME],
+      DESCRIPTION: rows[i][DESCRIPTION],
+      BASE_PRICE: rows[i][BASE_PRICE],
+      SUB_MODIFIER_NAME: rows[i][SUB_MODIFIER_NAME],
+      ITEMS: rows[i][ITEMS],
+      UPC: rows[i][UPC],
+      ITEMS_PRICE: rows[i][ITEMS_PRICE],
+    };
 
-    temp[ITEMS].push(item)
-
-   
+    temp[ITEMS].push(item);
   }
- 
-  result.push(temp);
 
- console.log(JSON.stringify(result,null,4));
-  
+  result.push(temp);
 
   return result;
 };
